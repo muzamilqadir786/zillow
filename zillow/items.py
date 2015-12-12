d2624e4a585914389aa0c96dc74e2dd64d46b495
muzamilqadir <muzamilqadir2@gmail.com>
2015-12-11 19:43:00 -0800

    Zillow scraper

---
+++ zillow/items.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ZillowItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    propertyUrl = scrapy.Field()
+    price = scrapy.Field()
+    zestimate = scrapy.Field()
+    ownerPhone = scrapy.Field()
+    city = scrapy.Field()
+    state = scrapy.Field()
+    zipcode = scrapy.Field()
+    street = scrapy.Field()
+    beds = scrapy.Field()
+    baths = scrapy.Field()
+    sqfts = scrapy.Field()
+    priceHistory = scrapy.Field()
+
+
+
